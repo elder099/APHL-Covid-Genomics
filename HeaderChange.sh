5f8fd229ba8dc4a899942464fe13549e80286a34
elder099 <elder099@umn.edu>
2021-06-03 16:27:08 -0500

    Quick fix

--- HeaderChange.sh
+++ HeaderChange.sh
@@ -13,5 +13,5 @@ echo $Suffix
 grep '^>[A-Za-z]' OCPH_Pass/OCPH1.fasta
 Numba= sed -n -e 's/^>[A-Za-z]*//p' OCPH_Pass/OCPH1.fasta   #Extracting the sample number programmatically
 
-echo fart
+echo break
 sed -n -e "1 s/^>[A-Za-z]*/$Prename/p" OCPH_Pass/OCPH1.fasta
