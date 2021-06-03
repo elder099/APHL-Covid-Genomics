79183e7bbc436ccb6fa5e4f07f1af02031e17b11
elder099 <elder099@umn.edu>
2021-06-03 16:24:38 -0500

    First round; adding HeaderChange code

---
+++ HeaderChange.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+Seq= seq 1 98      #sequence of sample #'s given in fastas
+Prename="hCoV-19\/USA\/UCI-"    #Prefix for GISAID virus name
+Suffix='/2021'
+
+
+
+echo $Seq
+echo $Prename
+echo $Suffix
+
+grep '^>[A-Za-z]' OCPH_Pass/OCPH1.fasta
+Numba= sed -n -e 's/^>[A-Za-z]*//p' OCPH_Pass/OCPH1.fasta   #Extracting the sample number programmatically
+
+echo fart
+sed -n -e "1 s/^>[A-Za-z]*/$Prename/p" OCPH_Pass/OCPH1.fasta
