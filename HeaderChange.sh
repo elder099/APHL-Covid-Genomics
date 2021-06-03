09915d9fdf14805a9dc9b8893f0e9b6288afdf92
elder099 <elder099@umn.edu>
2021-06-03 17:13:28 -0500

    printing new name

--- HeaderChange.sh
+++ HeaderChange.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
 Seq= seq 1 98      #sequence of sample #'s given in fastas
-Prename="hCoV-19\/USA\/UCI-"    #Prefix for GISAID virus name
+Prefix="hCoV-19\/USA\/UCI-"    #Prefix for GISAID virus name
 Suffix='/2021'
 
 
 
 echo $Seq
-echo $Prename
+echo $Prefix
 echo $Suffix
 
 grep '^>[A-Za-z]' OCPH_Pass/OCPH1.fasta
-Numba= sed -n -e 's/^>[A-Za-z]*//p' OCPH_Pass/OCPH1.fasta   #Extracting the sample number programmatically
+Numba=$(sed -n -e 's/^>[A-Za-z]*//p' OCPH_Pass/OCPH11.fasta)   #Extracting the sample number programmatically
+NumbaSize=${#Numba}
+
+echo $Numba
+echo $NumbaSize
+
+#Prepping for the while loop
+n=$NumbaSize
+NewNumba="$Numba"
+
+#While loop to prepend enough 0 digits
+#Each loop prepends a 0
+while [ $n -lt 3 ]
+do
+	NewNumba="0$NewNumba"
+	echo $NewNumba
+	n=$(( $n + 1 ))
+	echo $n
+done
 
 echo break
-sed -n -e "1 s/^>[A-Za-z]*/$Prename/p" OCPH_Pass/OCPH1.fasta
+
+Virus_Name="$Prefix$NewNumba$Suffix"
+echo $Virus_Name
+
+
+
