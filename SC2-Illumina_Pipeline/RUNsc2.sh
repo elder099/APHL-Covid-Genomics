833766cf7961adfb0baa7e2d07dd23350d383271
Ubuntu <ubuntu@ip-172-31-11-42.us-east-2.compute.internal>
2021-06-22 20:21:42 +0000

    quick path fix

--- SC2-Illumina_Pipeline/RUNsc2.sh
+++ SC2-Illumina_Pipeline/RUNsc2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 nextflow run czbiohub/sc2-illumina-pipeline -profile artic,docker \
-    --reads '../Basespace/Projects/20210514_SLO/Samples/SLOPH2/Files/*_R{1,2}_001.fastq.gz*' \
-    --kraken2_db 'sc2-illumina-pipeline/kraken2_db' \
-    --outdir '../sc2-output/20210514_SLO-output/'
+    --reads '../../Basespace/Projects/20210514_SLO/Samples/SLOPH2/Files/*_R{1,2}_001.fastq.gz*' \
+    --kraken2_db '../sc2-illumina-pipeline/kraken2_db' \
+    --outdir '../../sc2-output/20210514_SLO-output/'
