d14a4e7ba4cf0ae98e0138d3b96a8a0cea7893a0
Ubuntu <ubuntu@ip-172-31-11-42.us-east-2.compute.internal>
2021-06-29 18:38:42 +0000

    Add option to remove R_, do 6/21 run

--- GISAID_Upload/Start.sh
+++ GISAID_Upload/Start.sh
@@ -3,11 +3,20 @@
 #Found=$(find OCPH_pass -name "OCPH[1-3].fasta" -print) #Small subset for testing
 
 #Frances note to self:use this to change header for batch upload to GISAID
-for file in ~/workspace/SARS-CoV-2/20210621_SLO/consensus.fasta/2020/*.fasta
+for file in ~/workspace/SARS-CoV-2/20210621_SLO/consensus.fasta/2020_Fastas/*.fasta
 do
-	~/APHL-Covid-Genomics/GISAID_Upload/HeaderChange.sh -f "$file"
+	~/APHL-Covid-Genomics/GISAID_Upload/HeaderChange.sh -y 2020 -f "$file"
 done
 
 
+for file1 in ~/workspace/SARS-CoV-2/20210621_SLO/consensus.fasta/2021_Fastas/*.fasta
+do
+	~/APHL-Covid-Genomics/GISAID_Upload/HeaderChange.sh -y 2021 -f "$file1"
+done
 
-cat Fixed_Fasta/*.fasta > run4.fasta
+#Make each year separately just becaause
+cat Fixed_Fasta/2021_0621_SLO/2020_Fixed/*.fasta > Fixed_Fasta/2021_0621_SLO/Run4_2020.fasta
+cat Fixed_Fasta/2021_0621_SLO/2021_Fixed/*.fasta > Fixed_Fasta/2021_0621_SLO/Run4_2021.fasta
+
+#Merge the two years for a complete picture
+cat Fixed_Fasta/2021_0621_SLO/Run4_202[01].fasta > Fixed_Fasta/2021_0621_SLO/All_Run4.fasta     
