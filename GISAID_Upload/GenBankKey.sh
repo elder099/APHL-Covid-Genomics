8312960d443e4111305bc47acf97b995a703bb52
elder099 <elder099@umn.edu>
2021-08-31 14:49:48 -0500

    Quick Fix

--- GISAID_Upload/GenBankKey.sh
+++ GISAID_Upload/GenBankKey.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cat Fasta_Pieces/* > All_good_Aspen.fasta
-sed -n -e "/>CAOC[0-9]*/ s/$/ \[keyword\=purposeofsampling\:baselinesurveillance\]/p ; /[ACTGN][ACTGN][ACTGN][ACTGN]*/p" All_good_Aspen.fasta
+sed -n -e "/>CAOC[0-9]*/ s/$/ \[keyword\=purposeofsampling\:baselinesurveillance\]/p ; /[ACTGN][ACTGN][ACTGN][ACTGN]*/p" All_good_Aspen.fasta> All_good_Aspen.fasta
