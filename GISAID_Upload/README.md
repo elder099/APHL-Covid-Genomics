4baf734fa6ed1c83c4f7a57c70a70e68e0661b14
elder099 <elder099@umn.edu>
2021-09-14 15:02:51 -0500

    Surveillance_Tag update

--- GISAID_Upload/README.md
+++ GISAID_Upload/README.md
@@ -5,7 +5,7 @@ For Orange County:
   -**d** Date filepath in the form "YYYY-MM-DD"
   -**L** Shorthand name of the Lab "CA-OC-" (no input default is "CA-OC-")
 
-**Ex.** ./Start.sh -d SLO_2021-07-20 -L "CA-OC-" OR ./Start.sh -d SLO_2021-07-20
+**Ex.** ./Start.sh -d 2021-08-31 -L "CA-OC-" OR ./Start.sh -d 2021-08-31
 
 For San Luis Obispo:
   -**d** Date filepath in the form "SLO_YYYY-MM-DD"
--- Surveillance_Tags/CAOC_BL_Surveillance.R
+++ Surveillance_Tags/CAOC_BL_Surveillance.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 ###GenBank and GISAID IDs w/in-house sample names
 db_id<-read.csv("~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/resubs/CAOC_DB_IDs.csv",header=TRUE)
 ###Samples that are baseline surveillance w/in-house smaple names
@@ -19,6 +21,13 @@ bl_surveil_ids<-inner_join(bs_id,db_id,by=c("OCPHL" = "miseq_specimen_id"))
 bl_surveil_ids
 dim(db_id)[1]-57   #309 samples are surveillance
 sum(bl_surveil_ids$genbank_accession=="") #After removing non-surveillance samples, 12 were not accepted to Genbank
+sum(bl_surveil_ids$gisaid_accesion=="") #After removing non-surveillance samples, 1 was not accepted to GISAID
+
+
+gisaid_df<-bl_surveil_ids[bl_surveil_ids$gisaid_accesion!="",]
+gisaid_df<-gisaid_df[,c(2,4)]; gisaid_df
+
+#write.csv(gisaid_df,"~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/resubs/CAOC_Keywords_GISAID.csv",row.names=FALSE)
 
 
 ############
@@ -34,6 +43,7 @@ gb_id_merge<-inner_join(db_id,gb_ids,by=c("genbank_accession" = "Accession.ID"))
 dim(gb_id_merge); dim(db_id)[1]; 366 - 14  #This works out
 
 
+
 ############
 ###Merging baseline surveillance data w/known GenBank IDs
 Mix<-inner_join(bl_surveil_ids,gb_id_merge,by=c("genbank_accession")) #This is like removing 12 non-surveillance samples
@@ -42,6 +52,6 @@ Mix; dim(Mix); 309 - 12 #this matches 309 baseline samples excluding 12 that wer
 ###Final dataset only includes GenBank IDs and new Keyword
 Fin<-data.frame(genbank_accession=Mix$genbank_accession,Keyword=Mix$Keyword)
 Fin
-write.csv(Fin,"~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/resubs/CAOC_Keywords_GenBank.csv",row.names=FALSE)
+#write.csv(Fin,"~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/resubs/CAOC_Keywords_GenBank.csv",row.names=FALSE)
 
 
