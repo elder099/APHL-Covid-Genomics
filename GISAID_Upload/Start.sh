0c73b961ae8a73b6e35ed45edc8d88e62beab65f
elder099 <elder099@umn.edu>
2021-09-28 15:16:04 -0500

    Automate QC on main

--- GISAID_Upload/Start.sh
+++ GISAID_Upload/Start.sh
@@ -28,6 +28,16 @@ rm ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/Fixed_Fasta/*
 
 cat ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/Fasta_Pieces/* > ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/All_good_Aspen.fasta #Create Aspen-compatible file
 
-sed -n -e "/>[FCS][GAL][A-Z]*[0-9]*/ s/$/ \[keyword\=purposeofsampling\:baselinesurveillance\]/p ; /[ACTGN][ACTGN][ACTGN][ACTGN]*/p" ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/All_good_Aspen.fasta > ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/All_good_GB.fasta  #Add baseline surveillance tag to make GenBank-compatible file
+#sed -n -e "/>[FCS][GAL][A-Z]*[0-9]*/ s/$/ \[keyword\=purposeofsampling\:baselinesurveillance\]/p ; /[ACTGN][ACTGN][ACTGN][ACTGN]*/p" ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/All_good_Aspen.fasta > ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/All_good_GB.fasta  #Add baseline surveillance tag to make GenBank-compatible file
+
+
+#####Python section
+#Start conda environment
+. /Users/Gawdcomplex/opt/anaconda3/etc/profile.d/conda.sh
+
+#Run Assembly_QC
+mkdir -p ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/Assembly_QC  #Make sure directory is there
+python ../Assembly_QC/PercentCoverage.py -d $date_path
+
 
 sed -n -e 's/>//p' ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/All_good.fasta #Print out virus names for GISAID metadata
