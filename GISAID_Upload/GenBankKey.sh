f0f273c28a81ebfff197673a5fa087f04dbe01d9
elder099 <elder099@umn.edu>
2021-10-06 12:45:14 -0500

    Automate GenBank keyword addition

--- GISAID_Upload/GenBankKey.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cat Fasta_Pieces/* > All_good_pre_Aspen.fasta
-sed -n -e "/>CAOC[0-9]*/ s/$/ \[keyword\=purposeofsampling\:baselinesurveillance\]/p ; /[ACTGN][ACTGN][ACTGN][ACTGN]*/p" All_good_pre_Aspen.fasta > All_good_Aspen.fasta
-rm All_good_pre_Aspen.fasta
--- GISAID_Upload/Start.sh
+++ GISAID_Upload/Start.sh
@@ -19,16 +19,20 @@ mkdir -p ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/Fixed_Fasta
 
 echo "Seqtech supplied -- Clearlabs"
 
+###Run HeaderChange.sh on every fasta in date_path
 for file in ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/Fasta_Pieces/*.fa*
 do
 	./HeaderChange.sh -f "$file" -g $date_path -L $LAB -s $seqtech
 done
 
+
+#Create GISAID-formatted multi-fasta
 cat ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/Fixed_Fasta/*.fasta > ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/All_good.fasta
 rm ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/Fixed_Fasta/*
 
 
-cat ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/Fasta_Pieces/* > ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/All_good_Aspen.fasta #Create Aspen-compatible file
+#Create Aspen-compatible file
+cat ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/Fasta_Pieces/* > ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/All_good_Aspen.fasta 
 
 #sed -n -e "/>[FCS][GAL][A-Z]*[0-9]*/ s/$/ \[keyword\=purposeofsampling\:baselinesurveillance\]/p ; /[ACTGN][ACTGN][ACTGN][ACTGN]*/p" ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/All_good_Aspen.fasta > ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/All_good_GB.fasta  #Add baseline surveillance tag to make GenBank-compatible file
 
@@ -36,6 +40,9 @@ cat ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/Fasta_Pieces/* > ~/D
 #####Python section
 #Start conda environment
 . /Users/Gawdcomplex/opt/anaconda3/etc/profile.d/conda.sh
+
+#Create GenBank-compatible multi-fasta
+python GenBank_Addkey.py -d $date_path
 
 #Run Assembly_QC
 mkdir -p ~/Desktop/Covid_Genomics_APHL/GISAID_Uploads/$date_path/Assembly_QC  #Make sure directory is there
